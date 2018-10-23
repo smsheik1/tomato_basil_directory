937111f4a0558ed983412833b182eedcc0755761
“Shahzeb <shahzebsheikh54@gmail.com>
2018-10-22 19:10:45 -0500

    First Commit, uploading Social Media App called Twitter Fingers

---
+++ twitter_fingers/app/controllers/application_controller.rb
@@ -0,0 +1,14 @@
+class ApplicationController < ActionController::Base
+
+	before_action :authenticate_user!
+
+	before_action :configure_permitted_parameters, if: :devise_controller?
+
+	protected
+
+	def configure_permitted_parameters 
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :username, :bio, :location, :avatar])
+
+		devise_parameter_sanitizer.permit(:account_update, keys: [:name, :username, :bio, :location, :avatar])
+	end
+end
